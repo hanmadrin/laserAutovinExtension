d2c661eba3c8af0286d8f5193e19170a33e31f3d
hanmadrin <hanmadrin@gmail.com>
2024-01-15 16:06:25 +0600

    upgrade to monday API 2024-01

--- script.js
+++ script.js
@@ -1003,7 +1003,7 @@ const mondayFetch = async (query,apiVersion="2024-01") => {
                 'Content-Type': 'application/json',
                 'Authorization': 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE3MjU1MTMxNiwidWlkIjozMDI3MzE5NCwiaWFkIjoiMjAyMi0wNy0yN1QyMzowMzowNC4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6ODg0NzExMCwicmduIjoidXNlMSJ9.OsVnuCUSnm-FF21sjAND10cWEKN9-UIqIkNx6Rz8Bfo',
                 // 'Authorization' : 'eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjE1NTQ3NzM5NCwidWlkIjoyMTc2MjYwNiwiaWFkIjoiMjAyMi0wNC0xMlQxMzo0NjozOS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6ODg0NzExMCwicmduIjoidXNlMSJ9.mpXq7PtWbmneakwja8iB091bZFnElYif7Ji1IyBmmSA'
-                'API-Version' : apiVerison
+                'API-Version' : apiVersion
             },
             body: JSON.stringify({query})
         }
